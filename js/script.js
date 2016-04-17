b0fddbbe0877198960269430c9247002b2ec3cfb
John <johnfromkreaks@gmail.com>
2016-04-16 21:30:36 -0400

    Made the home page of the mobile website

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	
+	var count = 1;
+	$("header ul").hide();
+	$("#navigation_toggle").click(function() {
+		count++;
+		if (count % 2 === 0) {
+			$("header ul").show();
+			$("#navigation_toggle").addClass("active");
+			$("#navigation_toggle").removeClass("inactive");
+		} else {
+			$("header ul").slideUp();
+			$("#navigation_toggle").removeClass("active");
+			$("#navigation_toggle").addClass("inactive");
+		}
+	});
 });
