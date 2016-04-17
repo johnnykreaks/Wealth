2a038c8cad176f993b2749b467c7ca8e40a18a88
John <johnfromkreaks@gmail.com>
2016-04-17 18:48:50 -0400

    Completed the website

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
-	var count = 1;
-	$("header ul").hide();
-	$("#navigation_toggle").click(function() {
-		count++;
-		if (count % 2 === 0) {
+	function checkSize(){
+		if ($("#navigation_toggle").css("float") == "right" ){
+			var count = 1;
+			$("header ul").hide();
+			$("#navigation_toggle").click(function() {
+				count++;
+				if (count % 2 === 0) {
+					$("header ul").show();
+					$("#navigation_toggle").addClass("active");
+					$("#navigation_toggle").removeClass("inactive");
+				} else {
+					$("header ul").slideUp();
+					$("#navigation_toggle").removeClass("active");
+					$("#navigation_toggle").addClass("inactive");
+				}
+			});
+			$("#logo").click(function() {
+				count--;
+				$("header ul").hide();
+			});
+			$("ul a").click(function() {
+				count--;
+				$("header ul").hide();
+			});
+		} else if ($("#navigation_toggle").css("float") == "none" ){
 			$("header ul").show();
-			$("#navigation_toggle").addClass("active");
-			$("#navigation_toggle").removeClass("inactive");
-		} else {
-			$("header ul").slideUp();
-			$("#navigation_toggle").removeClass("active");
-			$("#navigation_toggle").addClass("inactive");
-		}
-	});
-	$("#logo").click(function() {
-		count--;
-		$("header ul").hide();
-	});
-	$("ul a").click(function() {
-		count--;
-		$("header ul").hide();
-	});
+		}	
+	}
+	checkSize();
+    $(window).resize(checkSize);
+	
 });
